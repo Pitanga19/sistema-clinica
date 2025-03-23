498c49d8fb1037926938f5501eda27dfb51b07b3
PitangaRack <lcmaciel99@gmail.com>
2025-03-23 10:39:20 -0300

    Crear esquemas para crear y retornar people

--- app/modules/people/schemas.py
+++ app/modules/people/schemas.py
@@ -1,18 +1,15 @@
 from pydantic import BaseModel, Field
 from typing import Annotated, Optional
 from app.db.tables.entities.schemas import EntityRead
-from app.db.tables.persons.schemas import PersonBase, PersonUpdate
+from app.db.tables.plans.schemas import PlanRead
+from app.db.tables.persons.schemas import PersonRead, PersonCreate, PersonUpdate
 from app.db.tables.patients.schemas import PatientUpdate
-
 
 class EntityOut(EntityRead):
     class Config:
         from_attributes = True
 
-class PlanOut(BaseModel):
-    id: Annotated[int, Field(..., gt=0)]
-    name: Annotated[str, Field(..., min_length=1)]
-    entity_id: Annotated[int, Field(..., gt=0)]
+class PlanOut(PlanRead):
     entity: EntityOut
 
     class Config:
@@ -28,20 +25,31 @@ class PatientOut(BaseModel):
     class Config:
         from_attributes = True
 
-class People(PersonBase):
-    id: Optional[Annotated[int, Field(gt=0)]] = None
-    patient: Optional[PatientOut]
+class PeopleOut(PersonRead):
+    patient: Optional[PatientOut] = None
     
     class Config:
         from_attributes = True
+
+class PatientFromPerson(BaseModel):
+    clinical_history_number: Annotated[int, Field(..., gt=0)]
+    entity_code: Annotated[str, Field(..., min_length=3, max_length=120)]
+    plan_id: Annotated[int, Field(..., gt=0)]
+
+    class Config:
+        from_attributes = True
+
+class PeopleCreate(PersonCreate):
+    id: Annotated[Optional[int], Field(..., gt=0)] = None
+    patient: Optional[PatientFromPerson] = None
 
 class PeopleUpdate(PersonUpdate, PatientUpdate):
     pass
 
 class PeopleFilter(BaseModel):
-    dni: Optional[Annotated[str, Field(min_length=1)]] = None
-    first_name: Optional[Annotated[str, Field(min_length=1)]] = None
-    last_name: Optional[Annotated[str, Field(min_length=1)]] = None
+    dni: Annotated[Optional[str], Field(min_length=1)] = None
+    first_name: Annotated[Optional[str], Field(min_length=1)] = None
+    last_name: Annotated[Optional[str], Field(min_length=1)] = None
     is_patient: Optional[bool] = None
-    entity_id: Optional[Annotated[int, Field(gt=0)]] = None
-    plan_id: Optional[Annotated[int, Field(gt=0)]] = None
+    entity_id: Annotated[Optional[int], Field(gt=0)] = None
+    plan_id: Annotated[Optional[int], Field(gt=0)] = None
