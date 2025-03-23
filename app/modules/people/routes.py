eb18fe7495f110da25cb83a8225bda13ec3bd197
PitangaRack <lcmaciel99@gmail.com>
2025-03-23 10:40:33 -0300

    Aplicar esquemas PeoppleCreate y PeopleOut

--- app/modules/people/routes.py
+++ app/modules/people/routes.py
@@ -3,40 +3,40 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from typing import List
 from app.db.session import get_db
 from app.modules.people import service
-from app.modules.people.schemas import People, PeopleUpdate, PeopleFilter
+from app.modules.people.schemas import PeopleCreate, PeopleOut, PeopleUpdate, PeopleFilter
 
 router = APIRouter(
     prefix='/people',
     tags=['People'],
 )
 
-@router.post('/', response_model=People, status_code=201)
+@router.post('/', response_model=PeopleOut, status_code=201)
 async def create_endpoint(
-    data: People,
+    data: PeopleCreate,
     db: AsyncSession=Depends(get_db)
-) -> People:
+) -> PeopleOut:
     return await service.create(data, db)
 
-@router.get('/{person_id}', response_model=People, status_code=200)
+@router.get('/{person_id}', response_model=PeopleOut, status_code=200)
 async def get_by_person_id_endpoint(
     person_id: int,
     db: AsyncSession = Depends(get_db)
-) -> People:
+) -> PeopleOut:
     return await service.get_by_person_id(person_id, db)
 
-@router.get('/', response_model=List[People], status_code=200)
+@router.get('/', response_model=List[PeopleOut], status_code=200)
 async def get_filtered_endpoint(
     filter: PeopleFilter = Depends(),
     db: AsyncSession = Depends(get_db)
-) -> List[People]:
+) -> List[PeopleOut]:
     return await service.get_filtered(filter, db)
 
-@router.patch('/{person_id}', response_model=People, status_code=200)
+@router.patch('/{person_id}', response_model=PeopleOut, status_code=200)
 async def update_endpoint(
     person_id: int,
     data: PeopleUpdate,
     db: AsyncSession=Depends(get_db)
-) -> People:
+) -> PeopleOut:
     return await service.update_patient(person_id, data, db)
 
 @router.delete('/remove-patient/{person_id}', status_code=204)
