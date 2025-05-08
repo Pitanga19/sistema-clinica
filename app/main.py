5a529eeb49e5611140ac34298e7e1e6ea10c353b
PitangaRack <lcmaciel99@gmail.com>
2025-05-08 12:38:59 -0300

    Importar settings y base

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
+from app.core.config import settings as sett
+from app.db import base
 
-app = FastAPI(title='Sistema Clínica', version='0.0.2')
+app = FastAPI(title=sett.PROJECT_NAME, version=sett.PROJECT_VERSION)
 
 @app.get('/', tags=['Home'])
 def root():
