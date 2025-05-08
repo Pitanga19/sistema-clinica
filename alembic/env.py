afb2c063a9c793ecbb6dea8f477ad63e9b9d3011
PitangaRack <lcmaciel99@gmail.com>
2025-05-08 13:26:38 -0300

    Importar modelos Role y Assignment

--- alembic/env.py
+++ alembic/env.py
@@ -8,6 +8,8 @@ from dotenv import load_dotenv
 from app.db.base_class import Base
 
 from app.db.tables.users.model import User
+from app.db.tables.roles.model import Role
+from app.db.tables.assignments.model import Assignment
 
 load_dotenv()
 config = context.config
--- app/db/base.py
+++ app/db/base.py
@@ -1,3 +1,5 @@
 from app.db.base_class import Base
 
 from app.db.tables.users.model import User
+from app.db.tables.roles.model import Role
+from app.db.tables.assignments.model import Assignment
