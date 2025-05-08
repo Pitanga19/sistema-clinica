0b6359cd4570a6dda8f33d05bd770f1e8842b3ac
Pitanga <lcmaciel99@gmail.com>
2025-05-08 18:20:12 -0300

    Importar excepciones CRUD y usar en app

--- app/main.py
+++ app/main.py
@@ -2,8 +2,12 @@ from fastapi import FastAPI
 from app.core.config import settings as sett
 from app.db import base
 from app.db.tables.routes_utils import collect_routers
+from app.db.tables.crud_utils import NotFoundError, AlreadyExistsError
 
 app = FastAPI(title=sett.PROJECT_NAME, version=sett.PROJECT_VERSION)
+
+app.add_exception_handler(NotFoundError, NotFoundError.handler)
+app.add_exception_handler(AlreadyExistsError, AlreadyExistsError.handler)
 
 @app.get('/', tags=['Home'])
 def root():
