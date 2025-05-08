9fae4e1cae3e1677b77f2dace3f6f502ee0c1a09
PitangaRack <lcmaciel99@gmail.com>
2025-05-08 15:24:04 -0300

    Crear esquemas para modelo User

---
+++ app/db/tables/users/schemas.py
@@ -0,0 +1,33 @@
+from pydantic import BaseModel, Field
+from typing import Annotated, Optional
+
+class UserBase(BaseModel):
+    role_id: Annotated[int, Field(..., gt=0)]
+    username: Annotated[str, Field(..., min_length=3, max_length=30)]
+    full_name: Annotated[str, Field(..., min_length=3, max_length=120)]
+    is_active: Annotated[bool, Field(default=True)]
+    is_superuser: Annotated[bool, Field(default=False)]
+    
+    model_config = {
+        'from_attributes': True,
+    }
+
+class UserCreate(UserBase):
+    id: Annotated[int, Field(..., gt=0, lt=10000)]
+    password: Annotated[str, Field(..., min_length=6, max_length=70)]
+
+class UserRead(UserBase):
+    id: Annotated[int, Field(..., gt=0, lt=10000)]
+
+class UserUpdate(BaseModel):
+    id: Annotated[Optional[int], Field(gt=0, lt=10000)] = None
+    username: Annotated[Optional[str], Field(min_length=3, max_length=30)] = None
+    password: Annotated[Optional[str], Field(min_length=6, max_length=70)] = None
+    full_name: Annotated[Optional[str], Field(min_length=3, max_length=120)] = None
+    role_id: Annotated[Optional[int], Field(gt=0)] = None
+    is_active: Optional[bool] = None
+    is_superuser: Optional[bool] = None
+
+class UserInDB(UserBase):
+    id: Annotated[int, Field(..., gt=0, lt=10000)]
+    hashed_password: Annotated[str, Field(..., min_length=6, max_length=255)]
