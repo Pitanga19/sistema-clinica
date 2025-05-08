4aea4f30dae1ba96785a8fdfafc07b212db8f0af
PitangaRack <lcmaciel99@gmail.com>
2025-05-08 12:36:58 -0300

    Crear clase Settings

---
+++ app/core/config.py
@@ -0,0 +1,14 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    PROJECT_NAME: str = 'Sistema Clínica'
+    PROJECT_VERSION: str = '0.0.2'
+    DATABASE_URL: str
+    JWT_SECRET: str
+    JWT_ALGORITHM: str
+    JWT_EXPIRATION_MINUTES: int
+
+    class Config:
+        env_file = '.env'
+
+settings = Settings()
