43806f4261891abe16dd25d66f77158692e8a136
Pitanga <lcmaciel99@gmail.com>
2025-05-08 17:39:02 -0300

    Importar y usar collect_routers en app

--- app/main.py
+++ app/main.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI
 from app.core.config import settings as sett
 from app.db import base
+from app.db.tables.routes_utils import collect_routers
 
 app = FastAPI(title=sett.PROJECT_NAME, version=sett.PROJECT_VERSION)
 
 @app.get('/', tags=['Home'])
 def root():
     return {'msg': 'Hola clinica'}
+
+for router in collect_routers():
+    app.include_router(router)
