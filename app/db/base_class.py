8c2fe017aa7f64cebfebc2bd796558eb58415699
Pitanga <lcmaciel99@gmail.com>
2025-05-08 23:07:31 -0300

    Remover __tablename__ automático

--- app/db/base_class.py
+++ app/db/base_class.py
@@ -2,6 +2,4 @@ from sqlalchemy.ext.declarative import as_declarative, declared_attr
 
 @as_declarative()
 class Base:
-    @declared_attr
-    def __tablename__(cls) -> str:
-        return cls.__name__.lower()
+    pass
