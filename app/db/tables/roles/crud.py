8123ebce54262c602d8ab3a758e132309f99a414
PitangaRack <lcmaciel99@gmail.com>
2025-05-09 10:35:08 -0300

    Modificar para usar modelo RoleAssignment, validar usuarios usando el rol antes de eliminar

--- app/db/tables/roles/crud.py
+++ app/db/tables/roles/crud.py
@@ -1,14 +1,17 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 from typing import List
+from app.core.exceptions import ValidationError
 from app.db.tables import crud_utils as utils
-from app.db.tables.roles.model import Role, role_assignments
+from app.db.tables.roles.model import Role
 from app.db.tables.roles.schemas import *
+from app.db.tables.assignments.model import Assignment
+from app.db.tables.users.crud import get_by_role_id as get_users_by_role_id
 
 async def create(data: RoleCreate, db: AsyncSession) -> Role:
     should_exist = False
     fields = [
-        ('nombre', data.name)
+        ('name', data.name)
     ]
     
     for field, value in fields:
@@ -31,13 +34,13 @@ async def get_by_id(id: int, db: AsyncSession) -> Role | None:
 async def get_by_name(name: str, db: AsyncSession) -> Role | None:
     stmt = select(Role).where(Role.name == name)
     should_exist = True
-    search_fields = [utils.SearchField(field='nombre', value=name)]
+    search_fields = [utils.SearchField(field='name', value=name)]
     return await utils.get_validated(stmt, should_exist, search_fields, db)
 
 async def get_by_assignment_id(assignment_id: int, db: AsyncSession) -> List[Role]:
     stmt = (select(Role)
-        .join(role_assignments)
-        .where(role_assignments.assignment_id == assignment_id)
+        .join(Role.assignments)
+        .where(Assignment.assignment_id == assignment_id)
     )
     return await utils.get_many(stmt, db)
 
@@ -54,4 +57,10 @@ async def update(id: int, data: RoleUpdate, db: AsyncSession) -> Role | None:
 
 async def delete(id: int, db: AsyncSession) -> None:
     role = await get_by_id(id, db)
+    
+    users = await get_users_by_role_id(id, db)
+    if users:
+        usernames = [user.username for user in users]
+        raise ValidationError(f'Hay usuarios asociados: {usernames}')
+    
     await utils.delete(role, db)
