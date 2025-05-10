717a127e23b7d9b9885f574a8eaac9f98c40fc94
Pitanga <lcmaciel99@gmail.com>
2025-05-10 01:58:21 -0300

    Importar y migrar tablas modes y entities

--- alembic/env.py
+++ alembic/env.py
@@ -11,6 +11,8 @@ from app.db.tables.users.model import User
 from app.db.tables.role_assignments.model import RoleAssignment
 from app.db.tables.roles.model import Role
 from app.db.tables.assignments.model import Assignment
+from app.db.tables.modes.model import Mode
+from app.db.tables.entities.model import Entity
 
 load_dotenv()
 config = context.config
--- app/db/base.py
+++ app/db/base.py
@@ -4,3 +4,5 @@ from app.db.tables.users.model import User
 from app.db.tables.role_assignments.model import RoleAssignment
 from app.db.tables.roles.model import Role
 from app.db.tables.assignments.model import Assignment
+from app.db.tables.modes.model import Mode
+from app.db.tables.entities.model import Entity
