10077aee38ebb66f0d8155d90c331d7478b11e8c
Pitanga <lcmaciel99@gmail.com>
2025-05-10 21:22:25 -0300

    Importar y migrar con alembic

--- alembic/env.py
+++ alembic/env.py
@@ -11,6 +11,7 @@ from app.db.tables.users.model import User
 from app.db.tables.role_assignments.model import RoleAssignment
 from app.db.tables.roles.model import Role
 from app.db.tables.assignments.model import Assignment
+from app.db.tables.professionals.model import Professional
 from app.db.tables.modes.model import Mode
 from app.db.tables.entities.model import Entity
 
