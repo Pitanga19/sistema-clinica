2852148249c432c087897f03365f57673a1f1c32
PitangaRack <lcmaciel99@gmail.com>
2025-05-12 10:39:03 -0300

    Corregir validaciones en update

--- app/db/tables/plans/crud.py
+++ app/db/tables/plans/crud.py
@@ -5,7 +5,8 @@ from app.core.exceptions import CustomError
 from app.db.utils import crud as utils
 from app.db.tables.plans.model import Plan
 from app.db.tables.plans.schemas import *
-from app.db.tables.patients.crud import get_by_plan_id
+from app.db.tables.entities.crud import get_by_id as get_entity_by_id
+from app.db.tables.patients.crud import get_by_plan_id as get_patients_by_plan_id
 
 async def create(data: PlanCreate, db: AsyncSession) -> Plan:
     stmt = select(Plan).where(
@@ -43,16 +44,17 @@ async def get_all(db: AsyncSession) -> List[Plan]:
 async def update(id: int, data: PlanUpdate, db: AsyncSession) -> Plan | None:
     plan = await get_by_id(id, db)
     should_exist = False
-    search_fields = [
-        ('name', data.name),            # por name
-        ('entity_id', data.entity_id)   # por ID
-    ]
     
     if data.name and data.entity_id:
+        await get_entity_by_id(data.entity_id, db)
         stmt = select(Plan).where(
             (Plan.name == data.name) &          # por name
             (Plan.entity_id == data.entity_id)  # por ID
         )
+        search_fields = [
+            utils.SearchField(field='name', value=data.name),           # por name
+            utils.SearchField(field='entity_id', value=data.entity_id), # por ID
+        ]
         await utils.get_validated(stmt, should_exist, search_fields, db)
         plan.name = data.name
         plan.entity_id = data.entity_id
@@ -62,8 +64,7 @@ async def update(id: int, data: PlanUpdate, db: AsyncSession) -> Plan | None:
             (Plan.entity_id == plan.entity_id)  # por ID
         )
         search_fields = [
-            ('name', data.name),            # por name
-            ('entity_id', data.entity_id)   # por ID
+            utils.SearchField(field='name', value=data.name),           # por name
         ]
         await utils.get_validated(stmt, should_exist, search_fields, db)
         plan.name = data.name
@@ -72,14 +73,17 @@ async def update(id: int, data: PlanUpdate, db: AsyncSession) -> Plan | None:
             (Plan.name == plan.name) &          # por name
             (Plan.entity_id == data.entity_id)  # por ID
         )
+        search_fields = [
+            utils.SearchField(field='entity_id', value=data.entity_id), # por ID
+        ]
         await utils.get_validated(stmt, should_exist, search_fields, db)
         plan.entity_id = data.entity_id
     
     return await utils.commit_and_refresh(plan, db)
 
 async def delete(id: int, db: AsyncSession) -> None:
-    patients = await get_by_plan_id(id, db)
-    if patients.length() > 0:
+    patients = await get_patients_by_plan_id(id, db)
+    if patients:
         raise CustomError('El plan no se puede eliminar porque tiene pacientes asignados')
     plan = await get_by_id(id, db)
     await utils.delete(plan, db)
