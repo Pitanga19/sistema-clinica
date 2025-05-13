544580d1b4844ed0dbee80acc63b0aa96a80bac5
PitangaRack <lcmaciel99@gmail.com>
2025-05-13 08:59:03 -0300

    Cambiar columna a tipo Text y migrar con alembic

--- app/db/tables/evaluations/model.py
+++ app/db/tables/evaluations/model.py
@@ -1,4 +1,4 @@
-from sqlalchemy import String, Integer, DateTime, ForeignKey
+from sqlalchemy import Text, Integer, DateTime, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from app.db.base_class import Base
 from datetime import datetime
@@ -8,7 +8,7 @@ class Evaluation(Base):
     __table_args__ = {'extend_existing': True}
     
     id: Mapped[int] = mapped_column(primary_key=True)
-    report: Mapped[str] = mapped_column(String)
+    report: Mapped[str] = mapped_column(Text)
     closed_at: Mapped[datetime] = mapped_column(DateTime, index=True, nullable=True)
     patient_id: Mapped[int] = mapped_column(Integer,ForeignKey('patients.id', name='evaluation_patient_id'), index=True, nullable=False)
     professional_id: Mapped[int] = mapped_column(Integer,ForeignKey('professionals.id', name='evaluation_professional_id'), index=True, nullable=False)
