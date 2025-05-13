c87de9fb71b416e4e9d8b15855a3a38d0e327e94
Pitanga <lcmaciel99@gmail.com>
2025-05-13 19:01:08 -0300

    Corregir href de login

--- front/src/services/api.ts
+++ front/src/services/api.ts
@@ -31,7 +31,7 @@ api.interceptors.response.use(
     (error) => {
         if (error.response && error.response.status === 401) {
             console.warn('No autorizado. Redirigiendo a login...')
-            window.location.href = '/login'
+            window.location.href = '/'
         }
         return Promise.reject(error)
     }
