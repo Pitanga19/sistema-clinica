2be3b46147068508235572668c08706917b98b16
PitangaRack <lcmaciel99@gmail.com>
2025-05-13 13:12:40 -0300

    Agregar ruta para cambiar contraseña

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi.security import OAuth2PasswordRequestForm
-from app.auth.schemas import Token
-from app.auth.services import get_token
+from app.auth.schemas import Token, TokenData, PasswordUpdate
+from app.auth.dependencies import get_current_user
+from app.auth.services import get_token, update_password
 from app.db.session import get_db
 
 router = APIRouter(
@@ -16,3 +17,11 @@ async def login_endpoint(
     db: AsyncSession = Depends(get_db),
 ):
     return await get_token(form_data, db)
+
+@router.patch('/password', response_model=Token)
+async def update_password_endpoint(
+    req: PasswordUpdate,
+    user_data: TokenData = Depends(get_current_user),
+    db: AsyncSession = Depends(get_db),
+):
+    return await update_password(req, user_data, db)
