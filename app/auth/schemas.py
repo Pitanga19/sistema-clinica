de465024335f88bccf81f4f74e90f9d3b3234b59
PitangaRack <lcmaciel99@gmail.com>
2025-05-13 11:16:13 -0300

    Crear esquemas de Tokens

---
+++ app/auth/schemas.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel, Field
+from typing import Annotated
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str = Field(default="bearer")
+
+class TokenData(BaseModel):
+    id: Annotated[int, Field(...)]
+    username: Annotated[str, Field(...)]
+    is_active: Annotated[bool, Field(...)]
+    is_superuser: Annotated[bool, Field(...)]
+    role_id: Annotated[int, Field(...)]
