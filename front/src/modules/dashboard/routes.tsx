c3c58b324eecd0cb07f8de53e6f1f342712a6d0a
Pitanga <lcmaciel99@gmail.com>
2025-05-13 19:44:54 -0300

    Agregar ruta para la página dashboard

---
+++ front/src/modules/dashboard/routes.tsx
@@ -0,0 +1,11 @@
+import type { RouteObject } from 'react-router-dom'
+import Dashboard from './pages/Dashboard'
+
+const dashboardRoutes: RouteObject[] = [
+    {
+        path: '/',
+        element: <Dashboard />,
+    }
+]
+
+export default dashboardRoutes
--- front/src/shared/AppRoutes.tsx
+++ front/src/shared/AppRoutes.tsx
@@ -1,9 +1,11 @@
 import { useRoutes } from 'react-router-dom'
 import authRoutes from '../modules/auth/routes'
+import dashboardRoutes from '../modules/dashboard/routes'
 
 const AppRoutes = () => {
     const routes = useRoutes([
         ...authRoutes,
+        ...dashboardRoutes,
     ])
 
     return routes
