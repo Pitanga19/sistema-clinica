e52e89de6f2008186819b0ee977c13d25e6bfe44
Pitanga <lcmaciel99@gmail.com>
2025-05-13 18:01:16 -0300

    Crear interceptores para manejar token y errores personalizados

--- front/src/services/api.ts
+++ front/src/services/api.ts
@@ -8,4 +8,32 @@ const api = axios.create({
     },
 })
 
+// Interceptor de request: agrega token JWT si existe
+api.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem('access_token')
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`
+        }
+        return config
+    },
+    (error) => {
+        return Promise.reject(error)
+    }
+)
+
+// Interceptor de response: maneja errores globalmente
+api.interceptors.response.use(
+    (response) => {
+        return response
+    },
+    (error) => {
+        if (error.response && error.response.status === 401) {
+            console.warn('No autorizado. Redirigiendo a login...')
+            window.location.href = '/login'
+        }
+        return Promise.reject(error)
+    }
+)
+
 export default api
