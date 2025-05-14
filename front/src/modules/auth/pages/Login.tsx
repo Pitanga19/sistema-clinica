499f7cb47d204d89602cf76a25fc6bca7797046f
PitangaRack <lcmaciel99@gmail.com>
2025-05-14 10:41:54 -0300

    Renombrar y mover service a raíz de auth

--- front/src/modules/auth/pages/Login.tsx
+++ front/src/modules/auth/pages/Login.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { login } from '../services/authService'
+import { login } from '../service'
 
 const Login = () => {
     const [username, setUsername] = useState('')
--- front/src/modules/auth/service.ts
+++ front/src/modules/auth/service.ts
@@ -1,4 +1,4 @@
-import api from '../../../shared/services/api'
+import api from '../../shared/services/api'
 
 export interface LoginData {
     username: string
