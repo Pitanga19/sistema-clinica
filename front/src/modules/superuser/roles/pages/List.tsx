70973747bc127151567c1a95e43de818ffd39673
Pitanga <lcmaciel99@gmail.com>
2025-05-14 16:20:07 -0300

    Agregar fuciones CRUD

--- front/src/modules/superuser/roles/pages/List.tsx
+++ front/src/modules/superuser/roles/pages/List.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 import { RoleService } from '../service'
 import type { Role } from '../types'
 import RolesListView from './List.view'
@@ -7,27 +8,49 @@ const RolesList = () => {
     const [roles, setRoles] = useState<Role[]>([])
     const [loading, setLoading] = useState(true)
     const [error, setError] = useState<string | null>(null)
+    const navigate = useNavigate()
+
+    const fetchRoles = async () => {
+        try {
+            const response = await RoleService.getAll()
+            setRoles(response.data)
+        } catch (error) {
+            setError(`${error}`)
+        } finally {
+            setLoading(false)
+        }
+    }
 
     useEffect(() => {
-        const fetchRoles = async () => {
-            try {
-                const response = await RoleService.getAll()
-                setRoles(response.data)
-            } catch (error) {
-                setError(`${error}`)
-            } finally {
-                setLoading(false)
-            }
-        }
-
         fetchRoles()
     }, [])
+
+    const handleCreate = () => navigate('/roles/create')
+
+    const handleView = (roleId: number) => navigate(`/roles/detail/${roleId}`)
+
+    const handleEdit = (roleId: number) => navigate(`/roles/update/${roleId}`)
+
+    const handleDelete = async (roleId: number) => {
+        try {
+            if (!confirm('Confirma que desea eliminar este rol?')) return
+            if (!confirm('Está seguro?')) return
+            if (!confirm('Muy seguro?')) return
+            await RoleService.delete(roleId)
+        } catch (error) {
+            setError(`${error}`)
+        }
+    }
 
     return (
         <RolesListView
             roles={roles}
             loading={loading}
             error={error}
+            onCreate={handleCreate}
+            onView={handleView}
+            onEdit={handleEdit}
+            onDelete={handleDelete}
         />
     )
 }
--- front/src/modules/superuser/roles/pages/List.view.tsx
+++ front/src/modules/superuser/roles/pages/List.view.tsx
@@ -1,15 +1,25 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faEye, faEdit, faTrash, faAdd } from "@fortawesome/free-solid-svg-icons"
 import type { Role } from "../types"
 
 interface RolesListViewProps {
     roles: Role[]
     loading: boolean
     error: string | null
+    onCreate: () => void
+    onView: (roleId: number) => void
+    onEdit: (roleId: number) => void
+    onDelete: (roleId: number) => void
 }
 
 const RolesListView = ({
     roles,
     loading,
     error,
+    onCreate,
+    onView,
+    onEdit,
+    onDelete,
 }: RolesListViewProps) => {
     let content
 
@@ -21,11 +31,27 @@ const RolesListView = ({
         content = <p>No hay roles para mostrar</p>
     } else {
         content = (
-            <ul>
-                {roles.map((role) => (
-                    <li key={role.id}>{role.name}</li>
-                ))}
-            </ul>
+            <div className='main_container'>
+                <button onClick={() => onCreate()}>
+                    Crear Rol <FontAwesomeIcon icon={faAdd}/>
+                </button>
+                <ul>
+                    {roles.map((role) => (
+                        <li key={role.id}>
+                            {role.name}
+                            <button onClick={() => onView(role.id)}>
+                                <FontAwesomeIcon icon={faEye}/>
+                            </button>
+                            <button onClick={() => onEdit(role.id)}>
+                                <FontAwesomeIcon icon={faEdit}/>
+                            </button>
+                            <button onClick={() => onDelete(role.id)}>
+                                <FontAwesomeIcon icon={faTrash}/>
+                            </button>
+                        </li>
+                    ))}
+                </ul>
+            </div>
         )
     }
 
