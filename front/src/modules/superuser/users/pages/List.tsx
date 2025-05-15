0fe01c642b7f1f6b0e8d9caf0f7152ff30ba13dc
Pitanga <lcmaciel99@gmail.com>
2025-05-14 21:37:17 -0300

    Crear lista funcional con opciones CRUD

--- front/src/modules/superuser/users/pages/List.tsx
+++ front/src/modules/superuser/users/pages/List.tsx
@@ -1,9 +1,58 @@
-import UsersListView from "./List.view"
+import { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { UserService } from '../service'
+import type { User } from '../types'
+import UsersListView from './List.view'
+import { userSnakeToCamel } from '../utils'
 
 const UsersList = () => {
+    const [users, setUsers] = useState<User[]>([])
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState<string | null>(null)
+    const navigate = useNavigate()
+
+    const fetchUsers = async () => {
+        try {
+            const response = await UserService.getAll()
+            const usersMapped = response.data.map((user) => userSnakeToCamel(user))
+            setUsers(usersMapped)
+        } catch (error) {
+            setError(`${error}`)
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    useEffect(() => {
+        fetchUsers()
+    }, [])
+
+    const handleCreate = () => navigate('/users/create')
+
+    const handleView = (userId: number) => navigate(`/users/detail/${userId}`)
+
+    const handleEdit = (userId: number) => navigate(`/users/update/${userId}`)
+
+    const handleDelete = async (userId: number) => {
+        try {
+            if (!confirm('Confirma que desea eliminar este usuario?')) return
+            if (!confirm('Está seguro?')) return
+            if (!confirm('Muy seguro?')) return
+            await UserService.delete(userId)
+        } catch (error) {
+            setError(`${error}`)
+        }
+    }
+
     return (
         <UsersListView
-            props={null}
+            users={users}
+            loading={loading}
+            error={error}
+            onCreate={handleCreate}
+            onView={handleView}
+            onEdit={handleEdit}
+            onDelete={handleDelete}
         />
     )
 }
--- front/src/modules/superuser/users/pages/List.view.tsx
+++ front/src/modules/superuser/users/pages/List.view.tsx
@@ -1,16 +1,63 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faEye, faEdit, faTrash, faAdd } from "@fortawesome/free-solid-svg-icons"
+import type { User } from "../types"
+
 interface UsersListViewProps {
-    props: null
+    users: User[]
+    loading: boolean
+    error: string | null
+    onCreate: () => void
+    onView: (userId: number) => void
+    onEdit: (userId: number) => void
+    onDelete: (userId: number) => void
 }
 
 const UsersListView = ({
-    props,
+    users,
+    loading,
+    error,
+    onCreate,
+    onView,
+    onEdit,
+    onDelete,
 }: UsersListViewProps) => {
-    console.log(props)
+    let content
+    console.log('Users: ', users)
+
+    if (loading) {
+        content = <p>Cargando users ...</p>
+    } else if (error) {
+        content = <p className='error'>{error}</p>
+    } else if (users.length === 0) {
+        content = <p>No hay users para mostrar</p>
+    } else {
+        content = (
+            <ul>
+                {users.map((user) => (
+                    <li key={user.id}>
+                        {user.fullName}
+                        <button onClick={() => onView(user.id)}>
+                            <FontAwesomeIcon icon={faEye}/>
+                        </button>
+                        <button onClick={() => onEdit(user.id)}>
+                            <FontAwesomeIcon icon={faEdit}/>
+                        </button>
+                        <button onClick={() => onDelete(user.id)}>
+                            <FontAwesomeIcon icon={faTrash}/>
+                        </button>
+                    </li>
+                ))}
+            </ul>
+        )
+    }
 
     return (
         <div className='main_container'>
-            <h1>Lista de Usuarios</h1>
-            <p>Seleccione el usuario que desea operar ...</p>
+            <button onClick={() => onCreate()}>
+                Crear Usuario <FontAwesomeIcon icon={faAdd}/>
+            </button>
+            <h1>Lista de Users</h1>
+            {content}
         </div>
     )
 }
