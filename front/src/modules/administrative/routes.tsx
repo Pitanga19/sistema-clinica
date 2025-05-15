55bfd71c5656772b1b78730777f736dbc19934a6
PitangaRack <lcmaciel99@gmail.com>
2025-05-15 12:21:34 -0300

    Importar rutas de CRUD de obras sociales

--- front/src/modules/administrative/routes.tsx
+++ front/src/modules/administrative/routes.tsx
@@ -3,12 +3,20 @@ import PersonsList from './persons/pages/List/List'
 import PersonsCreate from './persons/pages/Create/Create'
 import PersonsDetail from './persons/pages/Detail/Detail'
 import PersonsUpdate from './persons/pages/Update/Update'
+import EntitiesList from './entities/pages/List/List'
+import EntitiesCreate from './entities/pages/Create/Create'
+import EntitiesDetail from './entities/pages/Detail/Detail'
+import EntitiesUpdate from './entities/pages/Update/Update'
 
 const administrativeRoutes: RouteObject[] = [
     {path: '/persons', element: <PersonsList />},
     {path: '/persons/create', element: <PersonsCreate />},
     {path: '/persons/detail/:id', element: <PersonsDetail />},
     {path: '/persons/update/:id', element: <PersonsUpdate />},
+    {path: '/entities', element: <EntitiesList />},
+    {path: '/entities/create', element: <EntitiesCreate />},
+    {path: '/entities/detail/:id', element: <EntitiesDetail />},
+    {path: '/entities/update/:id', element: <EntitiesUpdate />},
 ]
 
 export default administrativeRoutes
