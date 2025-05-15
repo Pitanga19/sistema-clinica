3ead4a7cf51fd0a3883a4a5c1d592702d8ef6fec
Pitanga <lcmaciel99@gmail.com>
2025-05-14 21:36:48 -0300

    Corregir tipo de retorno de rutas del backend

--- front/src/modules/superuser/users/service.ts
+++ front/src/modules/superuser/users/service.ts
@@ -1,19 +1,19 @@
 import api from '../../../shared/services/api'
-import type { User, UserCreateBackend, UserUpdateBackend } from './types'
+import type { UserBackend, UserCreateBackend, UserUpdateBackend } from './types'
 
 export class UserService {
-    static create = (data: UserCreateBackend) => api.post<User>('/users', data)
+    static create = (data: UserCreateBackend) => api.post<UserBackend>('/users', data)
 
-    static getById = (id: number) => api.get<User>(`/users/by_id/${id}`)
+    static getById = (id: number) => api.get<UserBackend>(`/users/by_id/${id}`)
 
-    static getByUsername = (username: string) => api.get<User>(`/users/by_username/${username}`)
+    static getByUsername = (username: string) => api.get<UserBackend>(`/users/by_username/${username}`)
 
     static getByRoleId = (roleId: number) =>
-        api.get<User[]>(`/users/by_role_id/${roleId}`)
+        api.get<UserBackend[]>(`/users/by_role_id/${roleId}`)
 
-    static getAll = () => api.get<User[]>('/users')
+    static getAll = () => api.get<UserBackend[]>('/users')
 
-    static update = (id: number, data: UserUpdateBackend) => api.patch<User>(`/users/${id}`, data)
+    static update = (id: number, data: UserUpdateBackend) => api.patch<UserBackend>(`/users/${id}`, data)
 
     static delete = (id: number) => api.delete<void>(`/users/${id}`)
 }
