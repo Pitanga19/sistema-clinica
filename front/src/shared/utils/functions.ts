9a8e1275ed6bc0c9eb40a79e5b2561c87007ff81
PitangaRack <lcmaciel99@gmail.com>
2025-05-15 11:51:21 -0300

    Agregar tipado para columnas de la db, crear función para manejar nulls

--- front/src/shared/utils/functions.ts
+++ front/src/shared/utils/functions.ts
@@ -1,1 +1,7 @@
+import type { PropType } from "./types"
+
 export const booleanToString = (value: boolean): string => value ? 'Sí' : 'No'
+
+export const handleOptionalProp = (value: PropType): string => {
+    return (value !== null) ? value.toString() : 'Sin registro'
+}
---
+++ front/src/shared/utils/types.ts
@@ -0,0 +1,1 @@
+export type PropType = number | string | boolean | null
