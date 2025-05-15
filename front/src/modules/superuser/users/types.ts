4675cc66a2866639ac9b5d924e52054d18c88948
PitangaRack <lcmaciel99@gmail.com>
2025-05-15 10:31:07 -0300

    Eliminar tipado backend

--- front/src/modules/superuser/users/types.ts
+++ front/src/modules/superuser/users/types.ts
@@ -14,20 +14,3 @@ export interface UserCreate extends UserBase {
 }
 
 export type UserUpdate = Partial<UserCreate>
-
-export interface UserBaseBackend {
-    id: number
-    username: string
-    full_name: string
-    is_active: boolean
-    is_superuser: boolean
-    role_id: number
-}
-
-export type UserBackend = UserBaseBackend
-
-export interface UserCreateBackend extends UserBaseBackend {
-    password: string
-}
-
-export type UserUpdateBackend = Partial<UserCreateBackend>
