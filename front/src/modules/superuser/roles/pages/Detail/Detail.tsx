2917e18a80042996c79a1d3ddcaca60c9429bc2d
PitangaRack <lcmaciel99@gmail.com>
2025-05-15 08:52:08 -0300

    Agregar opción editar

--- front/src/modules/superuser/roles/pages/Detail/Detail.tsx
+++ front/src/modules/superuser/roles/pages/Detail/Detail.tsx
@@ -1,34 +1,44 @@
 import { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import { RoleService } from '../../service'
 import RolesDetailView from './Detail.view'
+import type { Role } from '../../types'
 
 const RolesDetail = () => {
     const { id } = useParams<{ id: string }>()
-    const [name, setName] = useState<string>('')
+    const [role, setRole] = useState<Role | null>(null)
     const [loading, setLoading] = useState<boolean>(true)
     const [error, setError] = useState<string | null>(null)
-    const loadingMsg = 'Cargando ...'
+    const navigate = useNavigate()
+    const loadingMsg = 'Cargando rol ...'
+
+    const handleEdit = (roleId: number) => navigate(`/roles/update/${roleId}`)
+
+    const fetchRole = async () => {
+        try {
+            if (!id) {
+                throw new Error('ID no proporcionado')
+            }
+            const response = await RoleService.getById(Number(id))
+            setRole(response.data)
+        } catch (error) {
+            setError(`${error}`)
+        } finally {
+            setLoading(false)
+        }
+    }
 
     useEffect(() => {
-        const fetchRole = async () => {
-            try {
-                if (!id) {
-                    throw new Error('ID no proporcionado')
-                }
-                const response = await RoleService.getById(Number(id))
-                setName(response.data.name)
-            } catch (error) {
-                setError(`${error}`)
-            } finally {
-                setLoading(false)
-            }
-        }
-
         fetchRole()
     }, [])
 
-    return <RolesDetailView name={name} loading={loading} loadingMsg={loadingMsg} error={error} />
+    return <RolesDetailView
+        role={role}
+        loading={loading}
+        loadingMsg={loadingMsg}
+        error={error}
+        onEdit={handleEdit}
+    />
 }
 
 export default RolesDetail
--- front/src/modules/superuser/roles/pages/Detail/Detail.view.tsx
+++ front/src/modules/superuser/roles/pages/Detail/Detail.view.tsx
@@ -1,20 +1,31 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEdit } from '@fortawesome/free-solid-svg-icons'
+import type { Role } from "../../types"
+
 interface RolesDetailViewProps {
-    name: string
+    role: Role | null
     loading: boolean
     loadingMsg: string
     error: string | null
+    onEdit: (roleId: number) => void
 }
 
 const RolesDetailView = ({
-    name,
+    role,
     loading,
     loadingMsg,
     error,
+    onEdit,
 }: RolesDetailViewProps) => {
+    if (!role) return
+
     return (
         <div className='main_container'>
             <h1>Detalles del Rol</h1>
-            <h2>{loading ? loadingMsg : name}</h2>
+            <h2>{loading ? loadingMsg : role.name}</h2>
+            <button onClick={() => onEdit(role.id)}>
+                <FontAwesomeIcon icon={faEdit} />
+            </button>
             {error && <p className='error'>{error}</p>}
         </div>
     )
