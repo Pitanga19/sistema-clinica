67a95f240c13fc0c09f37768a624614ca83bbb0e
Pitanga <lcmaciel99@gmail.com>
2025-05-18 03:15:50 -0300

    Agregar tipado y formulario común para crear y actualizar personas

--- front/src/modules/administrative/persons/types.ts
+++ front/src/modules/administrative/persons/types.ts
@@ -13,3 +13,23 @@ export type Person = PersonBase
 export type PersonCreate = PersonBase
 
 export type PersonUpdate = Partial<PersonCreate>
+
+export type PersonFormData = {
+    id: number
+    firstName: string
+    lastName: string
+    phone1: string
+    phone2: string | null
+    email: string | null
+    address: string
+}
+
+export const personDefaultData = {
+    id: 0,
+    firstName: '',
+    lastName: '',
+    phone1: '',
+    phone2: null,
+    email: null,
+    address: '',
+}
