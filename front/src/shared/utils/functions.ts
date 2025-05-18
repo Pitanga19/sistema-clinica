3d9a59d741edb1f4c13689e7b77f6ce1ec8e6193
Pitanga <lcmaciel99@gmail.com>
2025-05-18 03:15:19 -0300

    Corregir manejador de atributos opcionales, crear manejador de data modificada

--- front/src/shared/utils/functions.ts
+++ front/src/shared/utils/functions.ts
@@ -3,5 +3,17 @@ import type { PropType } from "./types"
 export const booleanToString = (value: boolean): string => value ? 'Sí' : 'No'
 
 export const handleOptionalProp = (value: PropType): string => {
-    return (value !== null) ? value.toString() : 'Sin registro'
+    return (value !== null && value !== undefined) ? value.toString() : 'Sin registro'
+}
+
+export const handleModifiedData = <T extends object>(data: T, defaultData: T): Partial<T> => {
+    const result: Partial<T> = {}
+
+    for (const key in data) {
+        if (data[key] !== defaultData[key]) {
+            result[key] = data[key]
+        }
+    }
+
+    return result
 }
