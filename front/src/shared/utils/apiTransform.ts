cfb5ba0aa90c2eda0aaef24d7699242a37b36ef9
Pitanga <lcmaciel99@gmail.com>
2025-05-18 03:14:31 -0300

    Corregir toSnake para transformar nombres de atributos con números

--- front/src/shared/utils/apiTransform.ts
+++ front/src/shared/utils/apiTransform.ts
@@ -1,5 +1,5 @@
 import camelcaseKeys from 'camelcase-keys'
-import decamelizeKeys from 'decamelize-keys'
+import decamelize from 'decamelize'
 
 type CamelInput = Record<string, unknown> | readonly Record<string, unknown>[]
 
@@ -8,5 +8,22 @@ export function toCamel<T>(data: CamelInput): T {
 }
 
 export function toSnake<T extends object>(data: T): unknown {
-    return decamelizeKeys(data, { deep: true })
+    const convert = (obj: unknown): unknown => {
+        if (Array.isArray(obj)) {
+            return obj.map(convert)
+        } else if (obj !== null && typeof obj === 'object') {
+            return Object.fromEntries(
+                Object.entries(obj).map(([key, value]) => [
+                    fixSnakeKey(key),
+                    convert(value),
+                ])
+            )
+        }
+        return obj
+    }
+
+    const fixSnakeKey = (key: string) =>
+        decamelize(key.replace(/(\d+)/g, '_$1'))
+
+    return convert(data)
 }
