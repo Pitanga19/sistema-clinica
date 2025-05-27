6c721adc34bd71970dcd8eb7dc0777d4ae571f03
PitangaRack <lcmaciel99@gmail.com>
2025-05-27 11:15:33 -0300

    Eliminar tabla Modes

--- alembic/env.py
+++ alembic/env.py
@@ -12,7 +12,6 @@ from app.db.tables.role_assignments.model import RoleAssignment
 from app.db.tables.roles.model import Role
 from app.db.tables.assignments.model import Assignment
 from app.db.tables.professionals.model import Professional
-from app.db.tables.modes.model import Mode
 from app.db.tables.entities.model import Entity
 from app.db.tables.plans.model import Plan
 from app.db.tables.persons.model import Person
