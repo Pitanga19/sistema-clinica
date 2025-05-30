5f0fa1d9a706d606b8bf989a7defccfd2c76f4cf
PitangaRack <lcmaciel99@gmail.com>
2025-05-30 13:31:16 -0300

    Agregar rutas de staff

--- app/core/api.py
+++ app/core/api.py
@@ -1,7 +1,9 @@
 from app.db.utils.routes import collect_db_routers
 from app.modules.auth.routes import router as auth_router
 from app.modules.people.routes import router as people_router
+from app.modules.staff.routes import router as staff_router
 
 routers = collect_db_routers()
 routers.append(auth_router)
 routers.append(people_router)
+routers.append(staff_router)
